b440a97604c4ca51f2373d7f54563bb57a00fda6
Ayush Gehlot <ayush_gehlot@hotmail.com>
2019-02-06 21:09:31 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -12,6 +12,6 @@ function happyHolidayTo(holiday, name){
   return `Happy ${holiday}, ${name}!`
 }
 
-function holidayCountdown(holiday, number){
+function holidayCountdown(number, holiday){
   `It's ${number} days until ${holiday}!`
 }
